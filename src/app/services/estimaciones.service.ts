4c96cc7b35444a1080f0ac65125d5ee758b4d5c2
josuetipan <133825763+josuetipan@users.noreply.github.com>
2024-10-05 15:51:33 -0500

    agrgadpo el servisi

--- src/app/services/estimaciones.service.ts
+++ src/app/services/estimaciones.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { Estimacion } from '../model/Estimacion';
+import { Data } from '../EstimacionData/Data';
 
 @Injectable({
   providedIn: 'root'
 })
-export class EstimacionesService {
- 
+export class EstimacionesService{
+  private estimacion:Estimacion=Data;
   constructor() { }
+
+  getEstimacion():Estimacion{
+    return this.estimacion
+  }
 }
